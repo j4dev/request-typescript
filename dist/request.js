dd282d9894d4f31eece5f34d8b1e98ce17e3cd6a
j4dev <jesusguanga98@outlook.com>
2019-11-07 16:01:44 -0500

    Peticon request get con fetch

---
+++ dist/request.js
@@ -0,0 +1,25 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function requestSWGastos() {
+    return __awaiter(this, void 0, void 0, function* () {
+        var url = "http://localhost/frontend/api/empresas/empresas.php";
+        const response = yield fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+        const json = yield response.json();
+        console.log(json);
+    });
+}
+;
+//# sourceMappingURL=request.js.map
